580d873e0a06ba48a2a1137b9483c651f08749c3
jieck <821242814@qq.com>
2020-08-20 20:15:42 +0800

    triangles

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green   = TGAColor(0, 255,   0,   255);
 
 void line_v1(int x0, int y0, int x1, int y1, TGAImage &image) {
     int yLen = std::abs(y1 - y0);
@@ -62,7 +63,7 @@ void line_v2(int x0, int y0, int x1, int y1, TGAImage &image) {
 }
 
 // quick version
-void line(int x0, int y0, int x1, int y1, TGAImage &image) {
+void line1(int x0, int y0, int x1, int y1, TGAImage &image) {
     int yLen = std::abs(y1 - y0);
     int xLen = std::abs(x1 - x0);
     bool steep = false;
@@ -93,18 +94,60 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image) {
     }
 }
 
-int main(int argc, char** argv) {
+// quick version
+void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color) {
+    int x0 = t0.x;
+    int y0 = t0.y;
+    int x1 = t1.x;
+    int y1 = t1.y;
+    int yLen = std::abs(y1 - y0);
+    int xLen = std::abs(x1 - x0);
+    bool steep = false;
+    if (xLen < yLen) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int derror = 0;
+    int dy = std::abs(y1 - y0);
+    int dx = (x1 - x0);
+    int y = y0;
+    for(int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        }else {
+            image.set(x, y, color);
+        }
+        derror += dy;
+        if (derror > dx) {
+            y += y1 - y0 > 0 ? 1 : -1;
+            derror -= dx;
+        }
+    }
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
+void drawFrame() {
     int lenght = 600;
-	TGAImage image(lenght, lenght, TGAImage::RGB);
-//	image.set(52, 41, red);
-//	image.set(52, 46, red);
-//	line(0, 0, lenght / 2, lenght / 2 - 100, image);
-//	line(0, 0, lenght / 2, lenght / 2 + 100, image);
-//	line(0, lenght / 2, lenght, lenght / 2, image); //
-//    line(0, lenght / 2, lenght, lenght / 2, image); //
-//	line(lenght / 2, 0, lenght / 2, lenght, image); //
-//	line(0, lenght, lenght, 0, image);
-//	line(0, 0, lenght, lenght, image);
+    TGAImage image(lenght, lenght, TGAImage::RGB);
+    //	image.set(52, 41, red);
+    //	image.set(52, 46, red);
+    //	line(0, 0, lenght / 2, lenght / 2 - 100, image);
+    //	line(0, 0, lenght / 2, lenght / 2 + 100, image);
+    //	line(0, lenght / 2, lenght, lenght / 2, image); //
+    //    line(0, lenght / 2, lenght, lenght / 2, image); //
+    //	line(lenght / 2, 0, lenght / 2, lenght, image); //
+    //	line(0, lenght, lenght, 0, image);
+    //	line(0, 0, lenght, lenght, image);
 	Model *m = new Model("C:\\Users\\yh\\pj\\p\\bin\\Debug\\african_head.obj");
     for(int i=0; i < m->nfaces(); i++) {
         std::vector<int> faces = m->face(i);
@@ -117,10 +160,33 @@ int main(int argc, char** argv) {
             int x1 = (end.x + 1) / 2 * lenght;
             int y1 = (end.y + 1) / 2 * lenght;
 //            std::cout<< x0 << " "<< y0 << " " << x1 << " " << y1 <<std::endl;
-            line(x0, y0, x1, y1, image);
+            line1(x0, y0, x1, y1, image);
         }
     }
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+}
+
+void fillTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+
+}
+
+void drawTriangles() {
+    int lenght = 600;
+    TGAImage image(lenght, lenght, TGAImage::RGB);
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, white);
+    triangle(t1[0], t1[1], t1[2], image, red);
+    triangle(t2[0], t2[1], t2[2], image, green);
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+}
+
+
+int main(int argc, char** argv) {
+    //drawFrame();
+    drawTriangles();
 	return 0;
 }
